2ca0bd21b6634238726c3d1a7e62ddcc487c82b3
letterpigeon <ban.luc@gmail.com>
2024-09-15 12:23:42 +0800

    Sort zeros, ones, twos

---
+++ gfg_solutions/sort_0_1_2.py
@@ -0,0 +1,32 @@
+"""
+Given an array arr containing only 0s, 1s, and 2s. Sort the array in ascending order.
+
+Examples:
+
+Input: arr[]= [0, 2, 1, 2, 0]
+Output: 0 0 1 2 2
+Explanation: 0s 1s and 2s are segregated into ascending order.
+Input: arr[] = [0, 1, 0]
+Output: 0 0 1
+Explanation: 0s 1s and 2s are segregated into ascending order.
+Expected Time Complexity: O(n)
+Expected Auxiliary Space: O(1)
+
+Constraints:
+1 <= arr.size() <= 106
+0 <= arr[i] <= 2
+"""
+class SortZeroOneTwos:
+    def sort_0_1_2(self, arr):
+        low, mid, high = 0, 0, len(arr) - 1
+        while mid <= high:
+            if arr[mid] == 0:
+                arr[low], arr[mid] = arr[mid], arr[low]
+                low += 1
+                mid += 1
+            elif arr[mid] == 1:
+                mid += 1
+            else:
+                arr[mid], arr[high] = arr[high], arr[mid]
+                high -= 1
+        return arr
